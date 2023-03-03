8e0354460a091af0db8a888631dada39a20d0861
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-03-03 19:01:10 +0900

    Update 이차반복4_문제02_단일출력_문제.py

--- I이차반복/이차반복4_문제_유동배열/이차반복4_문제02_단일출력_문제.py
+++ I이차반복/이차반복4_문제_유동배열/이차반복4_문제02_단일출력_문제.py
@@ -5,45 +5,42 @@
 	[정답]
 		1 2 3 4 100		
 '''
-a = [1,1,2,2,3,3,4,100,3]
+a = [1, 1, 2, 2, 3, 3, 4, 100, 3]
 
 
 print('===[2023-02-07 (화) #03]===')
-a = [1,1,2,2,3,3,4,100,3]
+a = [1, 1, 2, 2, 3, 3, 4, 100, 3]
 b = []
 
 for i in range(len(a)):
 
-	check = True
-	for j in range(len(b)):
-		if a[i] == b[j]:
-			check = False
-	
-	if check:
-		b.append(a[i])
-print(b)
+    check = True
+    for j in range(len(b)):
+        if a[i] == b[j]:
+            check = False
 
+    if check:
+        b.append(a[i])
+print(b)
 
 
 print('===[2023-02-07 (화) #02]===')
-a = [1,1,2,2,3,3,4,100,3]
+a = [1, 1, 2, 2, 3, 3, 4, 100, 3]
 b = []
 for v in a:
-	check = True
-	if v in b:
-		check = False
-	
-	if check:
-		b.append(v)
+    check = True
+    if v in b:
+        check = False
+
+    if check:
+        b.append(v)
 print(b)
 
 
-
 print('===[2023-02-07 (화) #01]===')
-a = [1,1,2,2,3,3,4,100,3]
+a = [1, 1, 2, 2, 3, 3, 4, 100, 3]
 
 a = set(a)
 a = list(a)
 a.sort()
 print(a)
-
