89778671c50181506d66a67fdae6cab710b2bb8c
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-03-07 18:29:18 +0900

    Update 이차반복4_문제03_로또정렬_문제.py

--- I이차반복/이차반복4_문제_유동배열/이차반복4_문제03_로또정렬_문제.py
+++ I이차반복/이차반복4_문제_유동배열/이차반복4_문제03_로또정렬_문제.py
@@ -21,4 +21,13 @@ def getList():
 		i += 1
 	return lotto
 
-print(getList())
+
+def bubbleSort(list):
+	for i in range(len(list) - 1):
+		for j in range(len(list) - i - 1):
+			if list[j] > list[j + 1]:
+				list[j], list[j + 1] = list[j + 1], list[j]
+	print(list)
+
+lotto = getList()
+bubbleSort(lotto)
