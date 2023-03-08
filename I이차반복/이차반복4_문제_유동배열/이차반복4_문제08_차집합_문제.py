ccf655573529d82a0fa346af1c341db0655c8b27
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-03-08 12:26:12 +0900

    Update 이차반복4_문제08_차집합_문제.py

--- I이차반복/이차반복4_문제_유동배열/이차반복4_문제08_차집합_문제.py
+++ I이차반복/이차반복4_문제_유동배열/이차반복4_문제08_차집합_문제.py
@@ -9,3 +9,53 @@ a = [16,  5, 11, 6, 19, 12, 18,  4, 20, 3]
 b = [17, 11, 19, 5, 13, 18, 16, 12, 11, 7]
 temp = []
 
+print('===[2023-03-08(수) #03]===')
+for i in range(len(a)):
+
+	isDuplicate = False
+	for j in range(len(b)):
+		if a[i] == b[j]:
+			isDuplicate = True
+			break
+
+	if isDuplicate == False:
+		temp.append(a[i])
+
+for i in range(len(b)):
+
+	isDuplicate = False
+	for j in range(len(a)):
+		if b[i] == a[j]:
+			isDuplicate = True
+			break
+
+	if isDuplicate == False:
+		temp.append(b[i])
+
+temp.sort()
+print(temp)
+
+
+print('===[2023-03-08(수) #02]===')
+temp = []
+for v in a:
+	if v not in b:
+		temp.append(v)
+
+for v in b:
+	if v not in a:
+		temp.append(v)
+
+temp.sort()
+print(temp)
+
+
+print('===[2023-03-08(수) #01]===')
+
+a = set(a)
+b = set(b)
+
+temp = list((a - b) | (b - a))
+temp.sort()
+
+print(temp)
