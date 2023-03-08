9322a79fcae9df1821d29980e591fbc47a3a6a01
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-03-08 12:26:21 +0900

    Update 이차반복4_문제11_혼자있는숫자_문제.py

--- I이차반복/이차반복4_문제_유동배열/이차반복4_문제11_혼자있는숫자_문제.py
+++ I이차반복/이차반복4_문제_유동배열/이차반복4_문제11_혼자있는숫자_문제.py
@@ -5,4 +5,17 @@
 		20 50
 '''
 
-a = [10,20,30,40,40,10,30,10,50]
+a = [10, 20, 30, 40, 40, 10, 30, 10, 50]
+
+print('===[2023-03-08(수)]===')
+
+for i in range(len(a)):
+
+	isAlone = True
+	for j in range(len(a)):
+		if i != j and a[i] == a[j]:
+			isAlone = False
+			break
+	
+	if isAlone:
+		print(a[i], end=" ")
