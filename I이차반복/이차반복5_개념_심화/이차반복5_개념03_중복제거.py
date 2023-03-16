447cd21a3f85237529fec14354eb1295246fd19a
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-03-16 14:09:54 +0900

    Update 이차반복5_개념03_중복제거.py

--- I이차반복/이차반복5_개념_심화/이차반복5_개념03_중복제거.py
+++ I이차반복/이차반복5_개념_심화/이차반복5_개념03_중복제거.py
@@ -8,19 +8,39 @@
 		b = [0, 3, 0, 0, 0, 50]
 '''
 
-a = [10,20,30,40,20]
-b = [10,3,20,10,20,50]
+a = [10, 20, 30, 40, 20]
+b = [10, 3, 20, 10, 20, 50]
+
+print("===[230316-4-1107]===")
 
 for i in range(len(a)):
-	check = -1
-	for j in range(len(b)):
-		if a[i] == b[j]:
-			check = a[i]
-			b[j] = 0
 
-	if check != -1:
-		for j in range(len(a)):
-			if check == a[j]:
-				a[j] = 0
+    isDuplicate = False
+
+    for j in range(len(b)):
+        if a[i] != 0 and a[i] == b[j]:
+            isDuplicate = True
+            b[j] = 0
+
+    if isDuplicate:
+        a[i] = 0
+
 print("a =", a)
 print("b =", b)
+
+
+# print("===[정답풀이]====")
+
+# for i in range(len(a)):
+# 	check = -1
+# 	for j in range(len(b)):
+# 		if a[i] == b[j]:
+# 			check = a[i]
+# 			b[j] = 0
+
+# 	if check != -1:
+# 		for j in range(len(a)):
+# 			if check == a[j]:
+# 				a[j] = 0
+# print("a =", a)
+# print("b =", b)
