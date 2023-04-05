d7e56eb3e2f15f7a95329cdfa7f075ea5629af9e
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-04-05 14:22:56 +0900

    230405-3-1422

--- I이차반복/이차반복5_문제_심화/이차반복5_문제02_개수세기_문제.py
+++ I이차반복/이차반복5_문제_심화/이차반복5_문제02_개수세기_문제.py
@@ -11,3 +11,31 @@ a = [10, 20, 30, 30, 100, 10, 30, 30, 20, 30, 20]
 
 value = []
 count = []
+
+print("===[230405-3-1412]===")
+
+for i in range(len(a)):
+	
+	isDuplicate = False
+	for j in range(len(value)):
+		if a[i] == value[j]:
+			isDuplicate = True
+			break
+
+	if isDuplicate:
+		continue
+	else:
+		value.append(a[i])
+
+print("value =", value)
+
+for i in range(len(value)):
+
+	cnt = 0
+	for j in range(len(a)):
+		if value[i] == a[j]:
+			cnt += 1
+
+	count.append(cnt)
+
+print("count =", count)
