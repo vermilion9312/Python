9a5dc27df86d8fb38d19a9fc18575f6ba9e49034
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-11-30 18:57:04 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,52 +1,19 @@
-# def game(user, oppnent):
+n = int(input())
+balloon = list(map(int, input().split()))
+arrow = [False for _ in range(n + 2)]
 
-#     userScore = 0
+firstBalloonHeight = balloon[0]
+arrow[firstBalloonHeight] = True
 
-#     if user == 'R':
-#         if oppnent == 'R':
-#             userScore += 1
-#         elif oppnent == 'S':
-#             userScore += 2
-#     if user == 'P':
-#         if oppnent == 'P':
-#             userScore += 1
-#         elif oppnent == 'R':
-#             userScore += 2
-#     if user == 'S':
-#         if oppnent == 'S':
-#             userScore += 1
-#         elif oppnent == 'P':
-#             userScore += 2
+for i in range(n - 1):
 
-#     return userScore
-    
-# r = int(input())
-# user = input()
-# n = int(input())
+	balloonHeight = balloon[i]
+	arrow[balloonHeight] = True
 
-# userScore = 0
-# maxUserScore = 0
-
-# for _ in range(n):
-
-#     oppnent = input()
-#     count = {'R': 0, 'P': 0, 'S':0}
-
-#     for i in range(r):
-#         userScore += game(user[i], oppnent[i])
-#         if oppnent[i] == 'R':
-#             count['R'] += 1
-#         elif oppnent[i] == 'P':
-#             count['P'] += 1
-#         else:
-#             count['S'] += 1
-
-    
-
-
-# print(userScore)
-# print(maxUserScore)
-
-var = 1
-var ^= 1
-print(var)
+	if balloonHeight - 1 == balloon[i + 1]:
+		arrow[balloonHeight] = False
+		arrow[balloonHeight - 1] = True
+	else:
+		arrow[balloonHeight] = True
+		
+print(arrow.count(True))
